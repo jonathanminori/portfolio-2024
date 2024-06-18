1da07b482096471ad9034839300c2496b8f9b945
Jonathan Minori <jonathanminori@users.noreply.github.com>
2024-06-18 13:51:20 -0700

    Initital setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['"PPNeueMontreal"', ...defaultTheme.fontFamily.sans]
       },
+      aspectRatio: {
+        '4/3': '4 / 3',
+        '3/4': '3 / 4'
+      },
+      width: {
+        '168': '42rem'
+      }
     },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        md: '0rem'
+      },
+      screens: {
+        lg: '42rem'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require('@tailwindcss/typography')]
+}
+export default config
