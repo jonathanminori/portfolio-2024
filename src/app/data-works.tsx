46548b6b98a78a10e34e3ee8e72fe1467d3c55fe
Jonathan Minori <jonathanminori@users.noreply.github.com>
2024-06-19 15:01:49 -0700

    Rough in of work gallery

---
+++ src/app/data-works.tsx
@@ -0,0 +1,19 @@
+import floating1 from '../../public/img/work/kfc/vr/ingame-1.webp'
+import floating2 from '../../public/img/work/kfc/vr/ingame-2.webp'
+import floating3 from '../../public/img/work/kfc/vr/ingame-3.webp'
+import floating4 from '../../public/img/work/kfc/vr/ingame-4.webp'
+import floating5 from '../../public/img/work/nike/live-design-ar/nike.jpg'
+import floating6 from '../../public/img/work/kfc/vr/ingame-4.webp'
+import floating7 from '../../public/img/work/nike/live-design-ar/nike.jpg'
+import floating8 from '../../public/img/work/kfc/vr/ingame-4.webp'
+
+export {
+  floating1,
+  floating2,
+  floating3,
+  floating4,
+  floating5,
+  floating6,
+  floating7,
+  floating8
+}
