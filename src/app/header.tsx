813c12fabeaf53ebc4484125649f2eecb5a06785
Jonathan Minori <jonathanminori@users.noreply.github.com>
2024-07-18 11:02:57 -0700

    changed the l/d mode button

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -36,7 +36,7 @@ export default function Header() {
   return (
     <header className='fixed right-0 top-0 z-30 px-8 py-4'>
       <nav>
-        <ul className='flex space-x-8'>
+        <ul className='mt-2 flex space-x-8'>
           {/* <li>
             <NavItem href='/' label='Profile' />
           </li>
@@ -44,7 +44,7 @@ export default function Header() {
           <li>
             <NavItem href='/works' label='Works' />
           </li> */}
-          <li className='mt-[2px]'>
+          {/* <li className='mt-[2px]'>
             <button
               onClick={() => toggleDarkMode()}
               className='cursor-pointer text-neutral-700 hover:opacity-60 dark:text-neutral-200'
@@ -55,6 +55,22 @@ export default function Header() {
               {!darkMode && (
                 <Moon size={13} strokeWidth={1.25} absoluteStrokeWidth />
               )}
+            </button>
+          </li> */}
+          <li>
+            <button onClick={() => toggleDarkMode()} className='group'>
+              <div className='relative m-2 aspect-square w-[10px] cursor-pointer overflow-hidden rounded-full opacity-70 outline outline-[1.5px] outline-offset-1 outline-neutral-800 transition-all duration-200 group-hover:opacity-100 dark:outline-neutral-200'>
+                {darkMode && (
+                  <div className='absolute left-1/2 top-0 h-full w-1/2 bg-neutral-200 transition-all duration-200 group-hover:left-0 group-hover:w-full'>
+                    <p className='sr-only'>Switch to light mode</p>
+                  </div>
+                )}
+                {!darkMode && (
+                  <div className='absolute left-0 top-0 h-full w-1/2 bg-neutral-800 transition-all duration-200 group-hover:w-full'>
+                    <p className='sr-only'>Switch to dark mode</p>
+                  </div>
+                )}
+              </div>
             </button>
           </li>
         </ul>
